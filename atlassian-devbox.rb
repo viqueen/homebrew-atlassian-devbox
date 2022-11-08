304d151c619a544a5f5efca18932a743d568e7e7
Hasnae <hasnae@viqueen.org>
2022-11-08 16:39:53 +1100

    upgrade to version 2.0.0

--- atlassian-devbox.rb
+++ atlassian-devbox.rb
@@ -3,15 +3,14 @@ require "language/node"
 class AtlassianDevbox < Formula
   desc "Atlassian enterprise products development tools"
   homepage "https://viqueen.org/notes/atlassian-devbox/"
-  url "https://registry.npmjs.org/atlassian-devbox/-/atlassian-devbox-1.0.2.tgz"
-  sha256 "4729255b8280f6363a78f8cbc910e32549a768003d5cc18c34dc375a862fb1a1"
+  url "https://registry.npmjs.org/atlassian-devbox/-/atlassian-devbox-2.0.0.tgz"
+  sha256 "2df83ba8b22ec6efa8c2e35882af5c4410df00ac29781e543c683673a92f6e31"
   license "Apache-2.0"
 
   depends_on "node"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    system "npm", "run", "postinstall"
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
