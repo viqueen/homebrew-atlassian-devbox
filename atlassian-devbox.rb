dffdc6e20d67653e826c4ce56c1899f4f7064132
viqueen <hasnae@viqueen.org>
2023-05-18 09:26:10 +1000

    upgrade to version 3.0.1

--- atlassian-devbox.rb
+++ atlassian-devbox.rb
@@ -3,8 +3,8 @@ require "language/node"
 class AtlassianDevbox < Formula
   desc "Atlassian enterprise products development tools"
   homepage "https://viqueen.org/notes/atlassian-devbox/"
-  url "https://registry.npmjs.org/atlassian-devbox/-/atlassian-devbox-3.0.0.tgz"
-  sha256 "ef5e7f0bc2e6da85464cdec03dabadb1ebea1a5052dfe7a0ddc3815f590ddf7f"
+  url "https://registry.npmjs.org/atlassian-devbox/-/atlassian-devbox-3.0.1.tgz"
+  sha256 "48b38793f5470cdcd86a0a2b68271df9c32a6ea3070733b0919e82d28742ff8a"
   license "Apache-2.0"
 
   depends_on "node"
